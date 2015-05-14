1c60a4b47ba2f353653ba541310a2b04fb852b29
lyz19890927 <lyz19890927@163.com>
2015-05-14 18:35:31 +0800

    检查基础数据的完成性

---
+++ com/tanry/checkIntegerity.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#-------------------------
+# Copyright (c) 2015
+# Tanry Electronic Technology Co., Ltd.
+# ChangSha, China
+# All Rights Reserved.
+# 功能：检查基础数据的完成性
+# 作者：liyzh
+# 时间：2015.5.14
+#-------------------------
+
+import initDeliveryType
+import initDeliveryUnit 
+import initMRPAmount
+import initShelfItem
+import initSupplierPrice
+import updateMainSupplier
